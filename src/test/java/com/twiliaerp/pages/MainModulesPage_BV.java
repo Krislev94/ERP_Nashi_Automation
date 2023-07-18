894a64074614778e6c9e32549db99a5973b35040
vorisib <vorisib>
2023-07-18 14:50:13 -0400

    Executed step definitions

--- src/test/java/com/twiliaerp/pages/MainModulesPage_BV.java
+++ src/test/java/com/twiliaerp/pages/MainModulesPage_BV.java
@@ -7,8 +7,18 @@ import java.util.List;
 
 public class MainModulesPage_BV extends BasePage {
 
-    @FindBy(xpath = "//li[@style='display: block;']")
+    //List of WebElements outside the dropdown
+    @FindBy(xpath = "//li[@id='menu_more_container']/preceding-sibling::li")
     public List<WebElement> menuTabs;
+
+    //List of WebElements inside the dropdown
+    @FindBy(xpath = "//li[@id='menu_more_container']//span")
+    public List<WebElement> moreTabs;
+
+    @FindBy(xpath = "(//*[@class='caret'])[1]")
+    public WebElement moreDropdown;
+
+
 
 
 
--- src/test/java/com/twiliaerp/step_definitions/US13_StepDefs_BV.java
+++ src/test/java/com/twiliaerp/step_definitions/US13_StepDefs_BV.java
@@ -1,13 +1,22 @@
 package com.twiliaerp.step_definitions;
 
 import com.twiliaerp.pages.LoginPage;
-import com.twiliaerp.utilities.ConfigurationReader;
+import com.twiliaerp.pages.MainModulesPage_BV;
 import com.twiliaerp.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class US13_StepDefs_BV {
     LoginPage loginPage = new LoginPage();
+    MainModulesPage_BV mainModulesPage_bv = new MainModulesPage_BV();
 
     @Given("the user logged in with valid {string} and {string} for POS manager")
     public void the_user_logged_in_with_valid_and_for_pos_manager(String email, String password) {
@@ -16,6 +25,22 @@ public class US13_StepDefs_BV {
 
     @Then("User should be able to access all {int} modules")
     public void userShouldBeAbleToAccessAllModules(int numberOfModules) {
+        System.out.println(mainModulesPage_bv.menuTabs.size());
+        //mainModulesPage_bv.menuTabs.forEach(webElement -> webElement.click());
+        for (WebElement menuTab : mainModulesPage_bv.menuTabs) {
+            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(15));
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[text()='Loading']")));
+            menuTab.click();
+        }
+        for (WebElement moreTab : mainModulesPage_bv.moreTabs) {
+            mainModulesPage_bv.moreDropdown.click();
+            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(15));
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[text()='Loading']")));
+            moreTab.click();
+            }
+            // WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(5));
+            // wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[text()='Loading']")));
+            Assert.assertEquals(mainModulesPage_bv.menuTabs.size(), numberOfModules);
 
+        }
     }
-}

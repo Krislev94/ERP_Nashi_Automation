d2a57675a6a20df1b31876fcb097345cbaf94517
dekir <dekkirozanj444@gmail.com>
2023-07-18 00:56:59 -0400

    Add repair POM and StepDefs

--- src/test/java/com/twiliaerp/pages/RepairModulePage_DD.java
+++ src/test/java/com/twiliaerp/pages/RepairModulePage_DD.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class RepairModulePage_DD {
 
     public RepairModulePage_DD(){
@@ -12,11 +14,16 @@ public class RepairModulePage_DD {
     }
 
 
-    @FindBy(xpath = "(//li[@class='active'])[1]/a")
+    @FindBy(xpath = "(//li[@class='active'])[1]/a/span")
     public WebElement repairsButton;
 
 
+    @FindBy(xpath = "(//input[@type='checkbox'])[3]")
+    public WebElement repairReferenceButton;
 
+
+    @FindBy(xpath = "//table//tbody//tr/td[1]//input")
+    public List<WebElement> allRepairs;
 
 
 }
--- src/test/java/com/twiliaerp/step_definitions/US12_StepDefs_DD.java
+++ src/test/java/com/twiliaerp/step_definitions/US12_StepDefs_DD.java
@@ -1,19 +1,27 @@
 package com.twiliaerp.step_definitions;
 
+import com.twiliaerp.pages.LoginPage;
 import com.twiliaerp.pages.RepairModulePage_DD;
+import com.twiliaerp.utilities.BrowserUtils;
 import com.twiliaerp.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
 
 public class US12_StepDefs_DD {
 
 
     RepairModulePage_DD repairModulePageDd = new RepairModulePage_DD();
+    LoginPage loginPage = new LoginPage();
 
     @Given("User is on the homepage")
     public void user_is_on_the_homepage() {
-        Driver.getDriver().get("https://qa.twiliaerp.com/web?#menu_id=115&action=120&active_id=channel_inbox");
+        Driver.getDriver().get("https://qa.twiliaerp.com/web?#view_type=list&model=mrp.repair&menu_id=535&action=723");
+        loginPage.emailInput.sendKeys("posmanager57@info.com");
+        loginPage.passwordInput.sendKeys("posmanager");
+        loginPage.loginButton.click();
 
     }
 
@@ -24,11 +32,19 @@ public class US12_StepDefs_DD {
 
     @When("User clicks Repair Reference checkbox")
     public void user_clicks_repair_reference_checkbox() {
+        BrowserUtils.sleep(5);
+        repairModulePageDd.repairReferenceButton.click();
+
+
 
     }
 
     @Then("User selects all repair orders")
     public void user_selects_all_repair_orders() {
+        for (WebElement each : repairModulePageDd.allRepairs){
+           Assert.assertTrue(each.isSelected());
+        }
+
 
     }
 }

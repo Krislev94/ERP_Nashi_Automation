2270fc4d03642b49118eaf4c50551eade74ef623
Yulia0904 <13neogroup13@gmail.com>
2023-07-19 10:39:55 -0400

    project is ready

--- src/test/java/com/twiliaerp/pages/LoginPage.java
+++ src/test/java/com/twiliaerp/pages/LoginPage.java
@@ -23,6 +23,11 @@ public class LoginPage extends BasePage{
     @FindBy(xpath = "//button[@type='submit']")
     public WebElement loginButton;
 
+    @FindBy(xpath = "//span[@class='oe_topbar_name']")
+    public WebElement accountName;
+
+    @FindBy(xpath = "//p[@class='alert alert-danger']")
+    public WebElement incorrectPasswordMessage;
 
     public void login(String email, String password) {
         emailInput.sendKeys(email);
